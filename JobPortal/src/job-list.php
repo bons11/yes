71462ea87933f4d77bf794b0d4b7a71f157308ae
sherwin <140298914+2smill@users.noreply.github.com>
2024-05-23 05:06:41 +0800

    ICON BUGALLON

--- JobPortal/src/job-list.php
+++ JobPortal/src/job-list.php
@@ -13,7 +13,7 @@ session_start(); // Start the session
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link href="img/bugallon-seal.png" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
