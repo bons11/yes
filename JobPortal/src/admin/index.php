7a622716b95292b3e3c648cee95bc22108ac10a4
sherwin <140298914+2smill@users.noreply.github.com>
2024-05-31 12:12:01 +0800

    Update Name

--- JobPortal/src/admin/index.php
+++ JobPortal/src/admin/index.php
@@ -11,7 +11,7 @@ include('../auth/php/config.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="img/bugallon-seal.png" rel="icon">
   <link rel="stylesheet" href="style/admin-style.css">
-  <title>Admin Login</title>
+  <title>Bugallon Municipal Bulletin Board</title>
 </head>
 <body>
   
